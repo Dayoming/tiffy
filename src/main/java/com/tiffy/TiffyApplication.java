40e2837f39e1675296d8da8c081de755270a07ad
정다영 <jdayoung1204@naver.com>
2024-10-31 18:44:48 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/tiffy/TiffyApplication.java
@@ -0,0 +1,13 @@
+package com.tiffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiffyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiffyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiffy/TiffyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiffy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiffyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
